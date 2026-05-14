e8ba30104198f2e73d3ef222c2ca81ba3cd900ea
Brandon M. Keltz <keltzbm@icloud.com>
2026-05-14 15:00:44 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ~/GitHub/dotfiles
+
+# Pull latest changes
+git pull
+
+# Stow everything
+stow --target=$HOME nvim
+stow --target=$HOME alacritty
+stow --target=$HOME starship
+stow --target=$HOME tmux
+stow --target=$HOME zsh
+
+echo "Dotfiles synced!"
