b5567ac4261337d93fd4e235f3f7e7807930cd9c
Brandon M. Keltz <keltzbm@icloud.com>
2026-05-14 17:10:11 -0600

    Update options.lua
    auto restore cursor positon

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -46,3 +46,13 @@ opt.listchars = {
 	precedes = "‹",
 	space = "·",
 }
+
+-- Restore cursor position when reopening a file
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		if mark[1] > 0 and mark[1] <= vim.api.nvim_buf_line_count(0) then
+			vim.api.nvim_win_set_cursor(0, mark)
+		end
+	end,
+})
